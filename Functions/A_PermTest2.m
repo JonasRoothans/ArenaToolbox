dbad1fb8c463aec9befe6592f6b6e01bd40bed83
Jonas <jroothans@gmail.com>
2020-04-24 13:41:16 +0200

    Improved permutation test

--- Functions/A_PermTest2.m
+++ Functions/A_PermTest2.m
@@ -1,4 +1,4 @@
-function [pValue, signedP] = A_PermTest2(sample1,sample2,p,other)
+function [pValue, signedP] = A_PermTest2(sample1,sample2,p,discriminative_statistic,other)
 %JR_2SAMPLEPERMUTATIONTEST Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,41 +6,62 @@ function [pValue, signedP] = A_PermTest2(sample1,sample2,p,other)
 if nargin==2
     p = 500000;
 end
-if nargin==4
+if nargin==5
     switch other
         case 'silent'
             verbose = 0;
         otherwise
             verbose = 1;
     end
-end
-[h,ptest,ci,stats]=ttest2(sample2, sample1);
-if verbose
-disp (['p will approximate : ', num2str(ptest)])
+else
+    verbose = 1;
 end
 
-test_statistic = abs(mean(sample1) - mean(sample2));
+if isempty(discriminative_statistic) || nargin==3
+    discriminative_statistic = 'mean';
+end
+
+
+switch discriminative_statistic
+    case 'mean'
+        [h,ptest,ci,stats]=ttest2(sample2, sample1);
+        test_statistic = abs(mean(sample1) - mean(sample2));
+    otherwise
+        error(['Discriminative method "',discriminative_statistic,'" does not exist.'])
+end
+
+end
+if verbose
+    disp (['p will approximate : ', num2str(ptest)])
+end
+
+
 
 mixed = [sample1(:);sample2(:)];
 tslist = zeros(1,p);
 if verbose
-tic
+    tic
 end
 for iP = 1:p
     if iP==100
-        t = toc;
+        
         if verbose
-        disp(['this will take approx. ',num2str(t/100*p),' seconds or ',num2str(t/100*p/60),' minutes'])
+            t = toc;
+            disp(['this will take approx. ',num2str(t/100*p),' seconds or ',num2str(t/100*p/60),' minutes'])
         end
     end
     shuffle = mixed(randperm(length(mixed)));
-    tslist(iP) = abs(mean(shuffle(1:numel(sample1))) - mean(shuffle(numel(sample1)+1:end)));
+    
+    switch discriminative_statistic
+        case 'mean'
+            tslist(iP) = abs(mean(shuffle(1:numel(sample1))) - mean(shuffle(numel(sample1)+1:end)));
+    end
 end
 
 pValue = sum(tslist>test_statistic) / p;
 signedP = (1-pValue) * sign(mean(sample1) - mean(sample2));
 if verbose
-disp(['precision: ',num2str(1/p)])
-disp('if signedP is positive, then sample A is bigger than sample B')
-toc;
+    disp(['precision: ',num2str(1/p)])
+    disp('if signedP is positive, then sample A is bigger than sample B')
+    toc;
 end
