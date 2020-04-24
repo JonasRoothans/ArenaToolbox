ede767aa6a061cfbb3006f26a6c760ad41f7e846
Jonas <jroothans@gmail.com>
2020-04-24 14:35:44 +0200

    permutation test extended with correlation

--- Functions/A_PermTest2.m
+++ Functions/A_PermTest2.m
@@ -2,7 +2,7 @@ function [pValue, signedP] = A_PermTest2(sample1,sample2,p,discriminative_statis
 %JR_2SAMPLEPERMUTATIONTEST Summary of this function goes here
 %   Detailed explanation goes here
 
-
+%%---------default values
 if nargin==2
     p = 500000;
 end
@@ -17,25 +17,36 @@ else
     verbose = 1;
 end
 
-if isempty(discriminative_statistic) || nargin==3
+if nargin==3
     discriminative_statistic = 'mean';
 end
+
+if isempty(discriminative_statistic) 
+    discriminative_statistic = 'mean';
+end
+%-----
 
 
 switch discriminative_statistic
     case 'mean'
         if verbose
             [h,ptest,ci,stats]=ttest2(sample2, sample1);
+            disp (['p will approximate : ', num2str(ptest)])
+            
         end
         test_statistic = abs(mean(sample1) - mean(sample2));
+        p_sign = sign(mean(sample1) - mean(sample2));
+    case 'correlation'
+        	[test_statistic,ptest] = corr(sample1(:),sample2(:));
+            disp (['p will approximate : ', num2str(ptest)])
+            p_sign = sign(test_statistic);
+            
     otherwise
         error(['Discriminative method "',discriminative_statistic,'" does not exist.'])
 end
 
-end
-if verbose
-    disp (['p will approximate : ', num2str(ptest)])
-end
+
+
 
 
 
@@ -53,17 +64,25 @@ for iP = 1:p
         end
     end
     shuffle = mixed(randperm(length(mixed)));
+    shuffle1 = sample1(randperm(length(sample1)));
     
     switch discriminative_statistic
         case 'mean'
             tslist(iP) = abs(mean(shuffle(1:numel(sample1))) - mean(shuffle(numel(sample1)+1:end)));
+        case 'correlation'
+            tslist(iP) = corr(shuffle1(:),sample2(:));
     end
 end
 
 pValue = sum(tslist>test_statistic) / p;
-signedP = (1-pValue) * sign(mean(sample1) - mean(sample2));
+signedP = (1-pValue) * p_sign;
 if verbose
     disp(['precision: ',num2str(1/p)])
-    disp('if signedP is positive, then sample A is bigger than sample B')
+    switch discriminative_statistic
+        case 'mean'
+            disp('if signedP is positive, then sample A is bigger than sample B')
+        case 'correlation'
+            disp('if signedP is positive, there is a positive correlation')
+    end
     toc;
 end
