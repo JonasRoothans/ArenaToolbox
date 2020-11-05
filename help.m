5ec5073db6255cbb72452f124880543e746290d6
Jonas <jroothans@gmail.com>
2020-11-05 10:50:03 +0100

    help opens the helpfigures

---
+++ help.m
@@ -0,0 +1,10 @@
+
+helpimg1 = imread('Arena-Classes v2020.11.png');
+figure;imshow(helpimg1)
+title('ArenaHelp: classes')
+
+
+
+helpimg2 = imread('Arena-Process v2020.11.png');
+f = figure;imshow(helpimg2)
+title('ArenaHelp: process')
