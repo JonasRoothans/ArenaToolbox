bf2483596f963a24b68f5ab644c548ef64479c0b
Jonas <jroothans@gmail.com>
2020-11-05 10:53:07 +0100

    help.m fix: clears variables after use

--- help.m
+++ help.m
@@ -6,5 +6,7 @@ title('ArenaHelp: classes')
 
 
 helpimg2 = imread('Arena-Process v2020.11.png');
-f = figure;imshow(helpimg2)
+figure;imshow(helpimg2)
 title('ArenaHelp: process')
+
+clear helpimg1 helpimg2
