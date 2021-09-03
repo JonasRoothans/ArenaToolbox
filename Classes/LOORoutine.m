92d273daba3cbd90729e734bbc6849d3b2c67438
Jonas <jroothans@gmail.com>
2021-09-03 17:12:10 +0200

    Revert "make sure to use shared voxels of left and right twice in analysis."
    This reverts commit 8e866bf20884f0eb364a63208b1667457e896b52.

--- Classes/LOORoutine.m
+++ Classes/LOORoutine.m
@@ -7,10 +7,8 @@ classdef LOORoutine < handle
     end
     
     properties (Hidden)
-        CleanPredictors
+        CleanHistograms
         LoadedMemory
-        SamplingMethod = 'Histogram' %or Average
-        NumberOfBins = 15;
     end
     
     
@@ -53,36 +51,23 @@ classdef LOORoutine < handle
                 LOO_VTA = obj.LoadedMemory.getVoxelDataAtPosition(iFilename);
                 
                 %take a bite
-                %--with value 1
-                sample = LOO_signedP.Voxels(and(LOO_VTA.Voxels>=1,LOO_tmap~=0));
-                %-- with value 2
-                %sample = [sample;LOO_signedP.Voxels(and(LOO_VTA.Voxels>=2,LOO_tmap~=0))];
-                
+                sample = LOO_signedP.Voxels(and(LOO_VTA.Voxels>0.5,LOO_tmap~=0));
                 
                 %analyse bite
-                switch obj.SamplingMethod
-                    case 'Histogram'
-                        edges = linspace(-1,1,obj.NumberOfBins+1);
-                        h = histogram(sample,edges);
-                        obj.CleanPredictors(iFilename,1:numel(edges)-1) = zscore(h.Values); 
-                        delete(h)
-                    case 'Average'
-                        obj.CleanPredictors(iFilename,1) = mean(sample);
-                    otherwise
-                       error('No valid method') 
-                       
-                end
-                
+                edges = -1:0.13333333333:1; % define the bins
+                h = histogram(sample,edges);
+                obj.CleanHistograms(iFilename,1:numel(edges)-1) = zscore(h.Values); 
+                delete(h)
                 
             end
             close(f)
             
-            obj.LOOmdl = fitlm(obj.CleanPredictors,obj.LoadedMemory.Weights); %Here it calculates the b (by fitting a linear model = multivariatelinearregression)
-            figure;obj.LOOmdl.plot
+            obj.LOOmdl = fitlm(obj.CleanHistograms,obj.LoadedMemory.Weights); %Here it calculates the b (by fitting a linear model = multivariatelinearregression)
+            figure;obj.LOOMdl.plot
         end
         
         function LOOCV(obj)
-            if isempty(obj.CleanPredictors)
+            if isempty(obj.CleanHistograms)
                 error('Run .LOOregression() first!')
             end
             
