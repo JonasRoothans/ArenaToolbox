3e809c5a0be32e16613d42f6f4ace68e60de8854
Jonas <jroothans@gmail.com>
2021-09-24 16:25:10 +0200

    New heatmap class that contains tmap signedpmap etc.

---
+++ Classes/Heatmap.m
@@ -0,0 +1,21 @@
+classdef Heatmap < handle
+    
+    properties
+        tmap
+        pmap
+        signedpmap
+        amap
+        cmap
+        rmap
+        raw
+        description
+    end
+    
+    methods
+        function obj = Heatmap()
+
+        end
+        
+    end
+end
+
