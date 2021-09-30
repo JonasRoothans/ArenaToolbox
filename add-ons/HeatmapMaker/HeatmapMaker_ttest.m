12e010fa731e6e2c0d3976a2fcecae0173593ce2
Jonas <jroothans@gmail.com>
2021-09-30 16:06:34 +0200

    better output management

--- add-ons/HeatmapMaker/HeatmapMaker_ttest.m
+++ add-ons/HeatmapMaker/HeatmapMaker_ttest.m
@@ -28,8 +28,11 @@ disp('Converting stack to heatmap.. this may take some minutes')
 heatmap = Stack.convertToHeatmapBasedOnVoxelValues(title,description);
 heatmap.Tag = nameSuggestion;
 disp('saving....')
-heatmap.save(fullfile(folder,nameSuggestion))
-heatmap.Signedpmap.savenii(fullfile(folder,nameSuggestion,'signedpmap.nii'))
+mkdir(fullfile(folder,nameSuggestion,'output'));
+heatmap.save(fullfile(folder,nameSuggestion,'output',nameSuggestion))
+heatmap.Signedpmap.savenii(fullfile(folder,nameSuggestion,'output','signedpmap.nii'))
+heatmap.Tmap.savenii(fullfile(folder,nameSuggestion,'output','Tmap.nii'))
+heatmap.Pmap.savenii(fullfile(folder,nameSuggestion,'output','Pmap.nii'))
 
 assignin('base','heatmap',heatmap)
 disp('heatmap is saved to harddisk and is available in workspace as ''heatmap''')
