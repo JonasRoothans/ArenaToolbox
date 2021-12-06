ecf09186202cd7628d47f3facf425dc3055a5dbb
Jonas <jroothans@gmail.com>
2021-12-06 17:13:45 +0100

    removed option 3b temporarily from heatmapmaker

--- add-ons/HeatmapMaker/install_HeatmapMaker.m
+++ add-ons/HeatmapMaker/install_HeatmapMaker.m
@@ -4,8 +4,8 @@ function install_HeatmapMaker(menuhandle,eventdata,scene)
 menuWithRecipe = scene.addon_addmenuitem('HeatmapMaker','Basic pipeline (exploratory)');
 scene.addon_addmenuitem('HeatmapMaker','1. Make recipe template from nii',str2func('@HeatmapMaker_makerecipe'),menuWithRecipe)
 scene.addon_addmenuitem('HeatmapMaker','2. Cook a heatmap',str2func('@HeatmapMaker_cook'),menuWithRecipe)
-scene.addon_addmenuitem('HeatmapMaker','3a. (Dirty) Regression',str2func('@HeatmapMaker_regress'),menuWithRecipe)
-scene.addon_addmenuitem('HeatmapMaker','3b. Leave One Out Cross Validation',str2func('@HeatmapMaker_LOOCV'),menuWithRecipe)
+scene.addon_addmenuitem('HeatmapMaker','3. (Dirty) Regression',str2func('@HeatmapMaker_regress'),menuWithRecipe)
+%scene.addon_addmenuitem('HeatmapMaker','3b. Leave One Out Cross Validation',str2func('@HeatmapMaker_LOOCV'),menuWithRecipe)
 
 prediction = scene.addon_addmenuitem('HeatmapMaker','Advanced pipeline (LOO)');
 scene.addon_addmenuitem('HeatmapMaker','Train a prediction algorithm (LOO)',str2func('@HeatmapMaker_trainPrediction'),prediction)
