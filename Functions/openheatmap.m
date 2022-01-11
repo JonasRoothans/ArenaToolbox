a25994cafc6278ce61fb5be169302c0e00a3041f
Jonas <jroothans@gmail.com>
2022-01-11 15:46:45 +0100

    extended usability of openheatmap

--- Functions/openheatmap.m
+++ Functions/openheatmap.m
@@ -2,8 +2,17 @@ function  openheatmap(filename)
 disp(['loading: ',filename])
 loaded = load(filename,'-mat');
 
-assignin('base','hm',loaded.hm)
-disp(['Heatmap ''',loaded.hm.Tag,''' is saved to the workspace as ''hm'''])
+fnames = fieldnames(loaded);
+if contains(fnames,'hm')
+    basename = ['hm_',loaded.hm.Tag];
+    assignin('base',['hm_',loaded.hm.Tag],loaded.hm)
+    disp(['Heatmap ''',loaded.hm.Tag,''' is saved to the workspace as ''',basename,''''])
+elseif contains(fnames,'heatmap')
+    basename = ['hm_',loaded.heatmap.Tag];
+    assignin('base',basename,loaded.heatmap)
+    disp(['Heatmap ''',loaded.heatmap.Tag,''' is saved to the workspace as ''',basename,''''])
+end
+
 
 end
 
