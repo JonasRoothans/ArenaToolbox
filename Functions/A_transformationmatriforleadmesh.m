54d94b6195aeca1ff5c6cf248dab4b20174d0b9e
Hazem-El-DeBakey <h.debakey@gmail.com>
2022-12-15 14:28:00 +0100

    gitignore-minor bug correction leadmesh transformation

--- Functions/A_transformationmatriforleadmesh.m
+++ Functions/A_transformationmatriforleadmesh.m
@@ -1,7 +1,11 @@
 function [T] = A_transformationmatriforleadmesh(location,zdirection, roll)
 %A_TRANSFORMATIONMATRIFORLEADMESH Trasformation from leadspace to world.
 zdirection = zdirection.unit.getArray()';
+if nargin<3
+    roll=0;
+end
 xdirection = cross([sin(deg2rad(roll)), cos(deg2rad(roll)), 0],zdirection);
+% xdirection = cross([0 1 0],zdirection);
 
 ydirection = cross(zdirection,xdirection);
 T = [xdirection/norm(xdirection);ydirection/norm(ydirection);zdirection/norm(zdirection);location.getArray()'];
