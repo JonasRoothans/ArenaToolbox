1c654f8c87dc1a7d01249ea932d4c073dad7f54e
Hazem-El-DeBakey <57719079+Hazem-El-DeBakey@users.noreply.github.com>
2023-05-22 17:02:53 +0200

    optimized CT based electrode reconstruction pipeline - brainlabextractor

--- add-ons/BrainlabExtractor/BrainlabExtractor_coreg.m
+++ add-ons/BrainlabExtractor/BrainlabExtractor_coreg.m
@@ -1,4 +1,4 @@
-function [outputArg1,outputArg2] = BrainlabExtractor_warp(master,inputfolder,outputfolder)
+function [outputArg1,outputArg2] = BrainlabExtractor_coreg(master,inputfolder,outputfolder,Excluded)
 %BRAINLABEXTRACTOR_WARP Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -7,13 +7,36 @@ function [outputArg1,outputArg2] = BrainlabExtractor_warp(master,inputfolder,out
         
         otherDir = {};
         for i = 1:numel(filenames)
-                slave =  [fullfile(filenames(i).folder,filenames(i).name),',1'];
+                movable_fn =  fullfile(filenames(i).folder,filenames(i).name);
+                if contains(filenames(i).name,string(Excluded))
+                    copyfile(movable_fn ,fullfile(filenames(i).folder,['r_',filenames(i).name]))
+                    continue
+                end
+                target =VoxelData(master);
+                movable= VoxelData(movable_fn);
+                movableCOG=movable.getcog;
+                targetCOG = target.getcog;
+                movecog = movableCOG - targetCOG;
+                
+                if any((movecog.x || movcog.y  || movecog.z) > 4)
+                    movablecopy=movable.copy;
+                    continue
+                else
+                    movablecopy = movable.copy;
+                    
+                    movablecopy.R.XWorldLimits = movablecopy.R.XWorldLimits-movecog.x;
+                    movablecopy.R.YWorldLimits = movablecopy.R.YWorldLimits-movecog.y;
+                    movablecopy.R.ZWorldLimits = movablecopy.R.ZWorldLimits-movecog.z;
+                end
+                movablecopy.savenii(fullfile(filenames(i).folder,filenames(i).name))
+                movable_input=[fullfile(filenames(i).folder,filenames(i).name), ',1'];
+                    
 
         
         
         %--- SPM SETTINGS        
         matlabbatch{1}.spm.spatial.coreg.estwrite.ref = {master};
-matlabbatch{1}.spm.spatial.coreg.estwrite.source = {slave};
+matlabbatch{1}.spm.spatial.coreg.estwrite.source = {movable_input};
 matlabbatch{1}.spm.spatial.coreg.estwrite.other = {''};
 matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.cost_fun = 'nmi';
 matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.sep = [4 2];
@@ -29,12 +52,13 @@ matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.prefix = 'r_';
         spm_jobman('run', matlabbatch);
 
         %--- moving files
+     
+        end
      [~, ~] = mkdir(outputfolder);
      mni_files = A_getfiles(fullfile(inputfolder,'r_*'));
      for iFile = 1:numel(mni_files)
          movefile(fullfile(mni_files(iFile).folder,mni_files(iFile).name),...
              fullfile(outputfolder,mni_files(iFile).name));
      end
-    end
 end
 
